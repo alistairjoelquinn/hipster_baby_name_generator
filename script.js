ee498623336a071b5015bed55ccdf230530ae4f5
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-02-28 10:52:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'k', 'l', 'm', 'n', 'r', 's', 't', 'w',];
+var rare = ['x', 'z', 'q', 'j', 'p', 'y', 'v'];
+
+var letters = document.getElementsByTagName('DIV');
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === ' ') {
+        for (let i = 0; i < letters.length; i++) {
+            const rando = Math.random();
+            if (rando > 0.5) {
+                letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+            } else if (rando < 0.35) {
+                letters[i].innerText = consonants[Math.floor(Math.random() * 14)];
+            } else {
+                letters[i].innerText = rare[Math.floor(Math.random() * 7)];
+            }
+        }
+    }
+});
