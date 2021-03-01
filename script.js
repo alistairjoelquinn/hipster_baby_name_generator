7f16a844c074462da43a2e3ca8d71cf81147bbe2
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-03-01 15:13:36 +0100

    update allows some consonants to follow t and s

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var consonantsOne = ['b', 'c', 'd', 'f', 'g', 'h', 'k', 'l', 'm', 'n', 'r', 's', 't', 'w',];
 var consonantsTwo = ['x', 'z', 'q', 'j', 'p', 'y', 'v'];
+tFollows = ['h', 'r', 'w'];
+sFollows = ['p', 'm', 'n', 't', 'c'];
 var tracker = [];
 
 var letters = document.getElementsByTagName('DIV');
@@ -13,35 +15,76 @@ function letterGet(e) {
                 if (tracker[0] === 'vowel') {
                     if (rando > 0.3) {
                         letters[i].innerText = consonantsOne[Math.floor(Math.random() * 14)];
+                        tracker.push('consonant');
                     } else {
                         letters[i].innerText = consonantsTwo[Math.floor(Math.random() * 7)];
+                        tracker.push('consonant');
                     }
                 } else {
-                    letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                    var rando2 = Math.random();
+                    if (letters[0].innerText.toLowerCase() === 't') {
+                        if (rando2 > 0.5) {
+                            letters[i].innerText = tFollows[Math.floor(Math.random() * 3)];
+                            tracker.push('consonant');
+                        } else {
+                            letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                            tracker.push('vowel');
+                        }
+                    } else if (letters[0].innerText.toLowerCase() === 's') {
+                        if (rando2 > 0.5) {
+                            letters[i].innerText = sFollows[Math.floor(Math.random() * 5)];
+                            tracker.push('consonant');
+                        } else {
+                            letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                            tracker.push('vowel');
+                        }
+                    } else {
+                        letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                        tracker.push('vowel');
+                    }
                 }
 
-            } else if (i === 3) {
-                if (tracker[1] === 'vowel') {
-                    if (rando > 0.3) {
+            } else if (i === 2) {
+                if (tracker[0] === 'consonant' && tracker[1] === 'consonant') {
+                    letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                    tracker.push('vowel');
+                } else {
+                    if (rando > 0.5) {
+                        letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                        tracker.push('vowel');
+                    } else if (rando < 0.4) {
                         letters[i].innerText = consonantsOne[Math.floor(Math.random() * 14)];
+                        tracker.push('consonant');
                     } else {
                         letters[i].innerText = consonantsTwo[Math.floor(Math.random() * 7)];
+                        tracker.push('consonant');
+                    }
+                }
+            } else if (i === 3) {
+                if (tracker[2] === 'vowel') {
+                    if (rando > 0.3) {
+                        letters[i].innerText = consonantsOne[Math.floor(Math.random() * 14)];
+                        tracker.push('consonant');
+                    } else {
+                        letters[i].innerText = consonantsTwo[Math.floor(Math.random() * 7)];
+                        tracker.push('consonant');
                     }
                 } else {
                     letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
+                    tracker.push('vowel');
                 }
             } else {
                 if (rando > 0.5) {
                     letters[i].innerText = vowels[Math.floor(Math.random() * 5)];
-                    tracker.push('vowel')
+                    tracker.push('vowel');
                 } else if (rando < 0.4) {
                     letters[i].innerText = consonantsOne[Math.floor(Math.random() * 14)];
-                    tracker.push('consonant')
+                    tracker.push('consonant');
                 } else {
                     letters[i].innerText = consonantsTwo[Math.floor(Math.random() * 7)];
+                    tracker.push('consonant');
                 }
             }
-
         }
         tracker = [];
     }
